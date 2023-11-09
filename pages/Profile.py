32231ea5d0af370aa3c0ddef115c59e7acb340fb
hibberdchristian <hibberdchristian@gmail.com>
2023-11-09 11:50:59 +0800

    Remove DB connection

--- pages/Profile.py
+++ pages/Profile.py
@@ -34,22 +34,14 @@ def main():
         index=['01-Jan','02-Feb','03-Mar','04-Apr','05-May','06-Jun','07-Jul','08-Aug','09-Sep','10-Oct','11-Nov','12-Dec']
         )
 
-    # Initialize database connection
-    conn = st.experimental_connection("mysql",type="sql")
-
-    df = conn.query("SELECT * from mytable;", ttl=600)
-
     st.title("Profile Page")
 
     # Display user profile
-
-    for user in df.itertuples():
-        st.subheader("User Profile")
-        st.write(f"Name: {st.session_state.name}")
-        st.write(f"Username: {st.session_state.username}")
-        st.write(f"Email: {user.email}")
-        st.write(f"Current Lanaguage Level: {user.language_level}")
-        st.write(f"Bio: {user.bio}")
+    st.subheader("User Profile")
+    st.write(f"Name: {st.session_state.name}")
+    st.write(f"Username: {st.session_state.username}")
+    st.write(f"Current Lanaguage Level: 82")
+    st.write(f"Bio: Passionate polyglot with an insatiable thirst for linguistic knowledge, constantly exploring new languages and cultures with boundless enthusiasm.")
 
     st.subheader("Learning Progress")
     st.area_chart(chart_data)
