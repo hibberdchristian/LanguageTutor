d8a7b23276b8be9e74a63fd6027513462e944632
hibberdchristian <hibberdchristian@gmail.com>
2023-11-23 14:09:21 +0800

    User accounts created

--- scripts/hash.py
+++ scripts/hash.py
@@ -1,5 +1,26 @@
 import streamlit_authenticator as stauth
 
 #insert passwords to be hashed -> open terminal and run Streamlit run scripts/hash.py
-hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
+hashed_passwords = stauth.Hasher([
+    'PolyglotP0wer!',
+    'LinguaL0ver$',
+    'Talk2MeInTongues',
+    'VerbNoun123',
+    'FluentFiesta!',
+    'WordWizard42',
+    'LingvoLandmarks',
+    'GrammarGuru99',
+    'VocabularyVoyage',
+    'LinguaLeap@2023',
+    'MultilingualMadness',
+    'CultureCraver#',
+    'LinguaLlama22',
+    'WordplayWonder!',
+    'LanguageLabyrinth$',
+    'SpeechSurfer123',
+    'LinguisticJourney',
+    'ConverseAndLearn',
+    'LexiconLover@',
+    'LinguaNova777'
+]).generate()
 print(hashed_passwords)
