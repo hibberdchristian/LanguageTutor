b4429fd36a378b5afd50c47685370502587f34a9
hibberdchristian <hibberdchristian@gmail.com>
2023-12-06 10:17:47 +0800

    User added

--- scripts/hash.py
+++ scripts/hash.py
@@ -2,6 +2,6 @@ import streamlit_authenticator as stauth
 
 #insert passwords to be hashed -> open terminal and run Streamlit run scripts/hash.py
 hashed_passwords = stauth.Hasher([
-    'VerbalVoyage123'
+    'VerbNoun#2023'
 ]).generate()
 print(hashed_passwords)
